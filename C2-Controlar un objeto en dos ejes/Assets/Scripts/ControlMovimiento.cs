f1db77660314106efa0ed5b12af632c197f88816
NDavidDuranM <20171056@ce.pucmm.edu.do>
2022-03-29 15:47:25 -0400

    C2 Completado

--- C2-Controlar un objeto en dos ejes/Assets/Scripts/ControlMovimiento.cs
+++ C2-Controlar un objeto en dos ejes/Assets/Scripts/ControlMovimiento.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class ControlMovimiento : MonoBehaviour
 {
-    float _speedY = 25f;
+    Vector3 _movementSpeed = new Vector3(25,25);
     Vector3 _deltaPos = new Vector3();
-    const float minLimiteY = -5.0f, maxLimiteY = 5.0f;
+    const float minLimiteY = -5.0f, maxLimiteY = 5.0f, minLimiteX = -5.0f, maxLimiteX = 5.0f;
 
     void Update()
     {
-        _deltaPos.y = Input.GetAxis("Vertical") * _speedY * Time.deltaTime;
+        _deltaPos.x = Input.GetAxis("Horizontal") * _movementSpeed.x;
+        _deltaPos.y = Input.GetAxis("Vertical") * _movementSpeed.y;
+        _deltaPos *= Time.deltaTime;
+
         gameObject.transform.Translate(_deltaPos);
+
         gameObject.transform.position = new Vector3
             (
-                gameObject.transform.position.x,
+                Mathf.Clamp(gameObject.transform.position.x, minLimiteX, maxLimiteX),
                 Mathf.Clamp(gameObject.transform.position.y, minLimiteY, maxLimiteY),
                 gameObject.transform.position.z
             );
